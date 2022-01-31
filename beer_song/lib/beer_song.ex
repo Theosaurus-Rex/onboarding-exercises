077a78b5bdc838554c2a9eb11a186e3c647b2b31
Theo Harris <theoharris@192-168-1-205.tpgi.com.au>
2022-02-01 08:48:59 +1100

    beer song case statement to functions, cheep socket working

--- beer_song/lib/beer_song.ex
+++ beer_song/lib/beer_song.ex
@@ -1,38 +1,34 @@
 defmodule BeerSong do
-  def verse(no_of_bottles) do
-    check_pluralisation(no_of_bottles)
+  @spec verse(non_neg_integer()) :: binary
+  def verse(2),
+    do: """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+
+  def verse(1),
+    do: """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+
+  def verse(0),
+    do: """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+
+  def verse(n) do
+    """
+    #{n} bottles of beer on the wall, #{n} bottles of beer.
+    Take one down and pass it around, #{n - 1} bottles of beer on the wall.
+    """
   end
 
-  def check_pluralisation(no_of_bottles) do
-    case no_of_bottles do
-      2 ->
-        """
-        #{no_of_bottles} bottles of beer on the wall, #{no_of_bottles} bottles of beer.
-        Take one down and pass it around, #{no_of_bottles - 1} bottle of beer on the wall.
-        """ 
-      1 ->
-        """
-        #{no_of_bottles} bottle of beer on the wall, #{no_of_bottles} bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-        """ 
-      0 ->
-        """
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
-        """ 
-      _ -> 
-        """
-        #{no_of_bottles} bottles of beer on the wall, #{no_of_bottles} bottles of beer.
-        Take one down and pass it around, #{no_of_bottles - 1} bottles of beer on the wall.
-        """ 
-    end
-  end
-
-  def lyrics(span \\ 99..0) do
-    verses_list = 
-    for verse <- span do
-      check_pluralisation(verse)
-    end
-    Enum.join(verses_list, "\n")
+  @spec lyrics(Enumerable.t()) :: String.t()
+  def lyrics(range \\ 99..0) do
+    range
+    |> Enum.map(&verse/1)
+    |> Enum.join("\n")
   end
 end
