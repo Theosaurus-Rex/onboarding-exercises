db7f2e92dcf6946dc60a9a1de71f4b8547510401
Theo Harris <theoharris@192-168-1-205.tpgi.com.au>
2022-02-01 12:12:40 +1100

    flight sim p1 complete

---
+++ l/lib/l.ex
@@ -0,0 +1,18 @@
+defmodule L do
+  @moduledoc """
+  Documentation for `L`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> L.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
