7bc06968aed93ea215c8b94942849a27b9753aa9
Theo Harris <theoharris@192-168-1-205.tpgi.com.au>
2022-02-03 13:20:37 +1100

    finish list functions exercise

--- l/lib/l.ex
+++ l/lib/l.ex
@@ -1,27 +1,44 @@
-
 defmodule L do
   def count(list), do: count(list, 0)
 
   def count([], acc), do: acc
-  def count([ _head | tail ], acc), do: count(tail, acc + 1)
+  def count([_head | tail], acc), do: count(tail, acc + 1)
 
-  def reverse(list), do: reverse_helper(list, [])
-  defp reverse_helper([], acc), do: acc
-  defp reverse_helper([ head | tail ], acc), do: reverse_helper(tail, [head | acc])
+  def reverse(list), do: do_reverse(list, [])
+  defp do_reverse([], acc), do: acc
+  defp do_reverse([head | tail], acc), do: do_reverse(tail, [head | acc])
 
-  def map([], _func), do: []
-  def map([ head | tail ], func), do: [ func.(head) | map(tail, func) ]
+  def map(list, fun), do: map(list, fun, [])
+  def map([], _fun, acc), do: reverse(acc)
+  def map([head | tail], fun, acc), do: map(tail, fun, [fun.(head) | acc])
 
-  def filter([], _fun), do: []
-  def filter([head | tail], func) do
-    if func.(head) do
-      [head | filter(tail, func)]
-    else
-      filter(tail, func)
-    end
+  def filter(list, fun), do: filter(list, fun, [])
+  def filter([], _fun, acc), do: reverse(acc)
+  def filter([head | tail], fun, acc) do
+    if(fun.(head), do: filter(tail, fun, [head | acc]), else: filter(tail, fun, acc))
   end
 
   def reduce([], value, _func), do: value
-  def reduce([ head | tail ], value, func), do: reduce(tail, func.(head, value), func)
+  def reduce([head | tail], value, func), do: reduce(tail, func.(head, value), func)
 
+  def append([], []), do: []
+  def append([], list), do: list
+  def append(list, []), do: list
+  def append(list1, list2), do: append(reverse(list1), list2, list2)
+  def append([], _list2, acc), do: acc
+  def append([h1 | t1], list2, acc) do
+    append(t1, list2, [h1 | acc])
+  end
+
+  def concat([]), do: []
+  def concat(list), do: concat([], [], list)
+  def concat(acc, [], []), do: reverse(acc)
+  # Once element is empty, process the rest
+  def concat(acc, rest, []), do: concat(acc, [], rest)
+  #Skip empty lists
+  def concat(acc, rest, [[] | tail]), do: concat(acc, rest, tail)
+  #Push the tail into the "rest" to separate out the head
+  def concat(acc, rest, [head | tail]) when is_list(head), do: concat(acc, [tail | rest], head)
+  #Add head to accumulator
+  def concat(acc, rest, [head | tail]), do: concat([head | acc], rest, tail)
 end
--- l/test/l_test.exs
+++ l/test/l_test.exs
@@ -14,11 +14,9 @@ defmodule LTest do
     assert L.count(Enum.to_list(1..1_000_000)) == 1_000_000
   end
 
-
   test "reverse of empty list" do
     assert L.reverse([]) == []
   end
-
 
   test "reverse of normal list" do
     assert L.reverse([1, 3, 5, 7]) == [7, 5, 3, 1]
@@ -28,98 +26,86 @@ defmodule LTest do
     assert L.reverse(Enum.to_list(1..1_000_000)) == Enum.to_list(1_000_000..1)
   end
 
-
   test "map of empty list" do
     assert L.map([], &(&1 + 1)) == []
   end
-
 
   test "map of normal list" do
     assert L.map([1, 3, 5, 7], &(&1 + 1)) == [2, 4, 6, 8]
   end
 
-
   test "map of huge list" do
     assert L.map(Enum.to_list(1..1_000_000), &(&1 + 1)) == Enum.to_list(2..1_000_001)
   end
 
-
   test "filter of empty list" do
     assert L.filter([], &Integer.is_odd/1) == []
   end
-
 
   test "filter of normal list" do
     assert L.filter([1, 2, 3, 4], &Integer.is_odd/1) == [1, 3]
   end
 
   test "filter of huge list" do
-    assert L.filter(Enum.to_list(1..1_000_000), &Integer.is_odd/1) == Enum.map(1..500_000, &(&1 * 2 - 1))
+    assert L.filter(Enum.to_list(1..1_000_000), &Integer.is_odd/1) ==
+             Enum.map(1..500_000, &(&1 * 2 - 1))
   end
-
 
   test "reduce of empty list" do
     assert L.reduce([], 0, &(&1 + &2)) == 0
   end
 
-
   test "reduce of normal list" do
     assert L.reduce([1, 2, 3, 4], -3, &(&1 + &2)) == 7
   end
-
 
   test "reduce of huge list" do
     assert L.reduce(Enum.to_list(1..1_000_000), 0, &(&1 + &2)) ==
              Enum.reduce(1..1_000_000, 0, &(&1 + &2))
   end
 
-
   test "reduce with non-commutative function" do
     assert L.reduce([1, 2, 3, 4], 10, fn x, acc -> acc - x end) == 0
   end
 
-  @tag :pending
   test "append of empty lists" do
     assert L.append([], []) == []
   end
 
-  @tag :pending
+
   test "append of empty and non-empty list" do
     assert L.append([], [1, 2, 3, 4]) == [1, 2, 3, 4]
   end
 
-  @tag :pending
+
   test "append of non-empty and empty list" do
     assert L.append([1, 2, 3, 4], []) == [1, 2, 3, 4]
   end
 
-  @tag :pending
+
   test "append of non-empty lists" do
     assert L.append([1, 2, 3], [4, 5]) == [1, 2, 3, 4, 5]
   end
 
-  @tag :pending
+
   test "append of huge lists" do
     assert L.append(Enum.to_list(1..1_000_000), Enum.to_list(1_000_001..2_000_000)) ==
              Enum.to_list(1..2_000_000)
   end
 
-  @tag :pending
+
   test "concat of empty list of lists" do
     assert L.concat([]) == []
   end
 
-  @tag :pending
   test "concat of normal list of lists" do
     assert L.concat([[1, 2], [3], [], [4, 5, 6]]) == [1, 2, 3, 4, 5, 6]
   end
 
-  @tag :pending
   test "concat of huge list of small lists" do
     assert L.concat(Enum.map(1..1_000_000, &[&1])) == Enum.to_list(1..1_000_000)
   end
 
-  @tag :pending
   test "concat of small list of huge lists" do
     assert L.concat(Enum.map(0..9, &Enum.to_list((&1 * 100_000 + 1)..((&1 + 1) * 100_000)))) ==
              Enum.to_list(1..1_000_000)
