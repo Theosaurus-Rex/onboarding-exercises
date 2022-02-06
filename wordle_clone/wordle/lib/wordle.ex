6eee720fa7ca2853d78d49ce1412fc947beb2e76
Theo Harris <theoharris@192-168-1-204.tpgi.com.au>
2022-02-07 10:13:01 +1100

    refactor wordle module

--- wordle_clone/wordle/lib/wordle.ex
+++ wordle_clone/wordle/lib/wordle.ex
@@ -1,20 +1,24 @@
 defmodule Wordle do
   def guess(player_guess, secret_word) do
-    secret_word_charlist = String.to_charlist(secret_word)
-    {result, _} = player_guess
-    |> String.to_charlist()
-    |> Enum.zip(secret_word_charlist)
-    |> Enum.reduce({[], secret_word_charlist}, fn {guess_letter, secret_letter}, {result, secret_letters} -> 
-      letter_result = letter_guess({guess_letter, secret_letter}, secret_letters) 
-      {[letter_result | result], secret_letters -- [guess_letter]}
-      end )
+    secret_letters = String.to_charlist(secret_word)
+
+    {result, _} =
+      player_guess
+      |> String.to_charlist()
+      |> Enum.zip(secret_letters)
+      |> Enum.reduce({[], secret_letters}, fn {guess_letter, secret_letter},
+                                                    {result, secret_letters} ->
+        letter_result = check_letter(guess_letter, secret_letter, secret_letters)
+        {[letter_result | result], secret_letters -- [guess_letter]}
+      end)
+
     Enum.reverse(result)
   end
 
-  def letter_guess({guess_letter, secret_letter}, secret_word) do
+  def check_letter(guess_letter, secret_letter, secret_letters) do
     cond do
       guess_letter == secret_letter -> {:correct, to_string([guess_letter])}
-      guess_letter in secret_word -> {:partial, to_string([guess_letter])}
+      guess_letter in secret_letters -> {:partial, to_string([guess_letter])}
       true -> {:incorrect, to_string([guess_letter])}
     end
   end
