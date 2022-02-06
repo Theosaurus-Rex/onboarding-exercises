c15a82f67333b64d787618ce32db3b35060aec0e
Theo Harris <theoharris@192-168-1-204.tpgi.com.au>
2022-02-07 08:11:42 +1100

    finish binary search guessing game example

--- guessing_game.ex
+++ guessing_game.ex
@@ -1,7 +1,11 @@
 defmodule GuessingGame do
   def guess(low, high) do
-    IO.puts(low)
-    IO.puts(high)
+    answer = IO.gets "Hmm...is your number #{mid(low, high)}?\n"
+    case String.trim(answer) do
+      "bigger" -> bigger(low, high)
+      "smaller" -> smaller(low, high)
+      "yes" -> "I knew I could guess your number!"
+    end
   end
 
   def mid(low, high) do
@@ -11,5 +15,10 @@ defmodule GuessingGame do
   def bigger(low, high) do
     new_low = min(high, mid(low, high) + 1)
     guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low, high) - 1)
+    guess(low, new_high)
   end
 end
